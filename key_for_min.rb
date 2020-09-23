d90b79cfc1bfa5ef666b0222e531241290442490
Amanda D <amanda@depaula.xyz>
2020-09-23 22:13:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  i = 0
-while i < name_hash.length do
-
-end
-if name_hash[i] == ""
-  return true
-    end
-    i += 1
+name.hash.any? do |lowest value|
   end
 end
