6018605522e625437980c737638d45f21786a090
Amanda D <amanda@depaula.xyz>
2020-09-23 22:08:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.keys
+  name_hash.count
 # name_hash.count do |foo, bar|
 #   if name_hash.count == lowest_value
 #   end
