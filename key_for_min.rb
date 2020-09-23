ca31207e334d2d1ff301f8b82566e989c4354bb5
Amanda D <amanda@depaula.xyz>
2020-09-23 23:06:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
 name_hash.each do |key|
   if name_hash[key] < lowest_value
 lowest_value = name_hash[key]
-lowest_value = key
+lowest_value = name_hash[key]
     end
   lowest_key
   end
