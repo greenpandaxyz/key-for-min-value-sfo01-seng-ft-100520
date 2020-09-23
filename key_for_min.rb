5fde1db6f817df67726bfdcdf79214d1156d4f3d
Amanda D <amanda@depaula.xyz>
2020-09-23 22:09:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_value = 0
-  name_hash.count do
+  name_hash.count do |lowest_value|
 if name_hash.count == lowest_value
   end
 end
